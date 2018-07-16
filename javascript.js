74bd20a5bad4acc636a547fe6c5334acb2d098d9
naz585 <naz585@aol.com>
2018-07-16 15:33:57 -0400

    JS addition/fixes

--- javascript.js
+++ javascript.js
@@ -2,29 +2,37 @@
 var hero;
 var boss;
 var warrior1 = {
-    hp: 200
+    hp: 200,
+    attack: 5
 };
 
 var mage1 = {
-    hp: 100
+    hp: 100,
+    attack: 10
 };
 
 var DK1 = {
-    hp: 150
-
+    hp: 150,
+    attack: 7
 };
 
 var boss3 = {
-    hp: 200
+    name: "boss3",
+    hp: 200,
+    attack: 20
 };
 
 var boss1 = {
-    hp: 100
+    name: "boss1",
+
+    hp: 100,
+    attack: 10
 };
 
 var boss2 = {
-    hp: 150
-
+    name: "boss2",
+    hp: 150,
+    attack: 15
 };
 
 
@@ -38,13 +46,16 @@ var bossPick = false;
 $(".btn1").hide();
 $(".boss").hide();
 
+
+
+
 $(".Hero").click(function () {
     characterSelected = event.target.alt;
     console.log(characterSelected);
     userPick = true;
     animate()
-    
-    boss()
+
+    theBoss()
 }
 
 );
@@ -58,10 +69,13 @@ function Gamestart() {
     if (userPick && bossPick) {
         console.log("gamestart test")
         $(".btn1").delay(2000).fadeIn("slow", function () {
-            // Animation complete
+
             attack();
+
         });
-        
+
+
+
     }
 
 };
@@ -73,6 +87,7 @@ function Gamestart() {
 function animate() {
     if (characterSelected === "warrior") {
         characterSelected = $(".war")
+        hero = warrior1;
         $(".mag, .DK , .magg, .DKK, .choose").fadeOut("slow", function () {
             $(this).remove();
             $(".warr").animate({ left: $(".warr").parent().width() / 2 - $(".warr").width() / 2 }, "slow");
@@ -81,6 +96,7 @@ function animate() {
     }
     else if (characterSelected === "mage") {
         characterSelected = $(".mag")
+        hero = mage1;
         $(".war, .DK , .warr, .DKK, .choose").fadeOut("slow", function () {
             $(".magg").addClass("mx-auto");
             $(this).remove();
@@ -89,6 +105,7 @@ function animate() {
     }
     else if (characterSelected === "DK") {
         characterSelected = $(".DK")
+        hero = DK1
         $(".war, .mag, .WA, .MA, .choose").fadeOut("slow", function () {
             $(this).remove();
             $(".DKK").animate({ right: $(".DKK").parent().width() / 2 - $(".DKK").width() / 2 }, "slow");
@@ -97,33 +114,36 @@ function animate() {
     }
     if (bossSelected === "Prinny") {
         bossSelected = $(".Prinny")
-        $(".Midboss, .Baal , .boss2, .boss3").fadeOut("slow", function () {
-            $(this).remove();
-            $(".boss1").animate({ left: $(".boss1").parent().width() / 2 - $(".boss1").width() / 2 }, "slow");
-            $(".Prinny").animate({ height: "200px", width: "200px" }, "slow");
-        });
+        boss = boss1;
+        $(".boss2, .boss3").fadeOut("slow")
+
+        $(".boss1").animate({ left: $(".boss1").parent().width() / 2 - $(".boss1").width() / 2 }, "slow");
+        $(".Prinny").animate({ height: "200px", width: "200px" }, "slow");
+
     }
     else if (bossSelected === "Midboss") {
         bossSelected = $(".Midboss")
-        $(".Prinny, .Baal , .boss1, .boss3").fadeOut("slow", function () {
-            $(".boss2").addClass("mx-auto");
-            $(this).remove();
-            $(".Midboss").animate({ height: "200px", width: "200px" }, "slow");
-        });
+        boss = boss2;
+        $(".boss1, .boss3").fadeOut("slow");
+        $(".boss2").addClass("mx-auto");
+
+        $(".Midboss").animate({ height: "200px", width: "200px" }, "slow");
+
     }
     else if (bossSelected === "Baal") {
         bossSelected = $(".Baal")
-        $(".Midboss, .Prinny").fadeOut("slow", function () {
-            $(this).remove();
-            $(".boss3").animate({ right: $(".boss3").parent().width() / 2 - $(".boss3").width() / 2 }, "slow");
-            $(".Baal").animate({ height: "200px", width: "200px" }, "slow");
-        });
+        boss = boss3;
+        $(".boss1, .boss2").fadeOut("slow")
+        $(".boss3").addClass("mx-auto");
+
+        $(".Baal").animate({ height: "200px", width: "200px" }, "slow");
+
     }
 
 
 }
 
-function boss() {
+function theBoss() {
     bossPick;
     bossSelected;
     $(".boss").delay(2500).fadeIn("slow", function () {
@@ -138,14 +158,38 @@ function boss() {
 
 }
 
-function attack(){
-    var attacker1 = DK1;
-    var attacker2 = boss1;
+function attack() {
+    var player = hero;
+    var opponant = boss;
+    var counter = 1;
     $(".attacker").click(function () {
-        attacker1.hp = attacker1.hp - 12
-        attacker2.hp = attacker2.hp - 6
-        console.log(attacker1.hp);
-        console.log(attacker2.hp);
+        counter++;
+        player.hp = player.hp - opponant.attack
+        opponant.hp = opponant.hp - (player.attack * counter)
+        console.log("counter: " + counter);
+        console.log(player.hp);
+        console.log(opponant.hp);
+        update();
+    });
 
-});
 };
+
+function update() {
+
+    if (boss.hp < 0) {
+        $("." + boss.name).fadeOut("slow");
+        $(".attacker").prop("disabled", true);
+    }
+    else {
+        $(".attacker").prop("disabled", false);
+    }
+
+
+
+};
+
+function show() {
+
+
+
+}
